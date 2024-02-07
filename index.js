77b6768df7cbf91739683fd8160e2ef8e712d61a
John <johntheherrera@gmail.com>
2024-02-06 21:30:11 -0800

    ag

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const startButton = document.getElementById('start');
 const mainPage = document.getElementById('main_page');
+const video1 = document.getElementById('vid1');
+const video2 = document.getElementById('vid2');
+const prepButton = document.getElementById('two');
+const prepText = document.getElementById('prepText');
+
+let clapsound = new Audio('static/audio/clap.mp3');
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 startButton.addEventListener('click', function() {
     // Remove Front Page
     startButton.parentNode.removeChild(startButton);
     mainPage.innerHTML = '';
-    mainPage.parentNode.removeChild(allDiv);
-    document.documentElement.style.height = '100vh';
-    document.body.style.height = '100vh';
 
     // Start Game
-    gameStart();
+    gamePrep();
 })
 
+function clapSound() {
+    clapsound.play();
+}
+
+// Game Prep
+
+async function gamePrep() {
+    prepButton.classList.remove('hide');
+}
 
 // Actual Game Start
 
-function gameStart() {
+async function gameStart() {
 
+    prepButton.removeChild(prepText);
+
+    video1.style.display = 'block';
+    video2.style.display = 'block';
+
+    await sleep(1000);
+
+    video1.play();
+    video2.play();
 }
